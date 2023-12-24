724720703be0a69d6e668645c4df16135df18fd2
javabitpro <102875546+javabitpro@users.noreply.github.com>
2023-12-24 07:56:15 +0700

    Create javabitprojs.js

---
+++ javabitprojs.js
@@ -0,0 +1,11 @@
+  <script>
+    $('.case').each(function() {
+  var link = $(this).html();
+  $(this).contents().wrap('<a target="_blank" href="https://www.youtube.com/@javabitpro-editor/videos"></a>');
+});
+
+ $('.case1').each(function() {
+  var link = $(this).html();
+  $(this).contents().wrap('<a target="_blank" href="https://www.javabitpro.com/"></a>');
+});
+    </script>
