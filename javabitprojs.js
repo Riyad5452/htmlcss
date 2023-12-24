e9a424326e5d019ed7022930a07e985bc2abb170
javabitpro <102875546+javabitpro@users.noreply.github.com>
2023-12-24 08:10:36 +0700

    Update javabitprojs.js

--- javabitprojs.js
+++ javabitprojs.js
@@ -1,11 +1,7 @@
-<script>
-    $('.case').each(function() {
-  var link = $(this).html();
-  $(this).contents().wrap('<a target="_blank" href="https://www.youtube.com/@javabitpro-editor/videos"></a>');
-});
+  <script>
 
- $('.case1').each(function() {
-  var link = $(this).html();
-  $(this).contents().wrap('<a target="_blank" href="https://www.javabitpro.com/"></a>');
-});
-</script>
+    function openGoogleByMethod(){
+    window.open("https://www.google.com")
+   }
+
+    </script>
